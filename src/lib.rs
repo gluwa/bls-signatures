665a4323dc0a050a0d818856f7245707f98faf17
Beqa Abuladze <beqaabuladze.00@gmail.com>
2024-11-29 18:37:55 +0400

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+mod error;
+pub mod key;
+pub mod signature;
+
+pub use self::error::Error;
+pub use self::key::{PrivateKey, PublicKey, Serialize};
+pub use self::signature::{
+    aggregate, hash, verify, verify_agg_message, verify_messages, Signature,
+};
+
+#[cfg(test)]
+extern crate base64_serde;
